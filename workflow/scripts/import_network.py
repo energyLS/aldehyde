f139501ac84001d53b59379bb32ef413bcb1576f
energyls <leon1.schumm@oth-regensburg.de>
2022-10-17 12:40:00 +0100

    feat: import and delete ev ex RE

--- workflow/scripts/import_network.py
+++ workflow/scripts/import_network.py
@@ -1,9 +1,29 @@
 import pypsa
 import os
+import pandas as pd
+
+def remove_non_electric_buses(n):
+    """
+    remove buses from pypsa-eur with carriers which are not AC buses
+    """
+    print("drop buses from network with carrier: ", n.buses[~n.buses.carrier.isin(["AC", "DC"])].carrier.unique())
+    n.buses = n.buses[n.buses.carrier.isin(["AC", "DC"])]
+
+    return
 
 
+def remove_elec_base_techs(n):
+    """remove conventional generators (e.g. OCGT) and storage units (e.g. batteries and H2)
+    from base electricity-only network, since they're added here differently using links
+    """
 
-
+    for c in n.iterate_components(snakemake.config["pypsa_earth"]):
+        to_keep = snakemake.config["pypsa_earth"][c.name]
+        to_remove = pd.Index(c.df.carrier.unique()).symmetric_difference(to_keep)
+        print("Removing", c.list_name, "with carrier", to_remove)
+        names = c.df.index[c.df.carrier.isin(to_remove)]
+        n.mremove(c.name, names)
+        n.carriers.drop(to_remove, inplace=True, errors="ignore")
 
 
 
@@ -17,6 +37,13 @@ if __name__ == "__main__":
         sets_path_to_root('aldehyde')
 
 
-    # Read European and Moroccan pypsa networks
+    # Read Morocco network
     # https://pypsa.readthedocs.io/en/latest/components.html?highlight=override_component_attrs#custom-components
-    n_eur = pypsa.Network(snakemake.input.network) #, override_component_attrs=overrides)
+    n = pypsa.Network(snakemake.input.network) #, override_component_attrs=overrides)
+
+
+    remove_elec_base_techs(n)
+    remove_non_electric_buses(n)
+    #clean_network(n)
+
+    n.export_to_netcdf(snakemake.output[0])
